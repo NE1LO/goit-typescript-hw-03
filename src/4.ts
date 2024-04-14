c6bb52446eee5cbf91bd713c62658e755addd4da
Anton Smeiko <asmeiko13@gmail.com>
2024-04-14 14:42:02 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,35 @@
+class Key {
+  signature: number = Math.random();
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  comeIn(person: Person) {
+    if (this.door) this.tenants.push(person);
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +38,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
